162032de77bbdf4510bea0bc5055acea4ee073fa
Elizabeth Merritt <lmerritt11@gmail.com>
2017-05-07 11:30:50 -0400

    changes to homepage - ideal image slider

--- js/javascript.js
+++ js/javascript.js
@@ -2,3 +2,10 @@ $(document).ready(function(){
   $(".owl-carousel").owlCarousel();
   console.log()
 });
+
+var slider = new IdealImageSlider.Slider({
+  selector: '#slider',
+  height: auto, // Required but can be set by CSS
+  interval: 4000
+});
+slider.start();
