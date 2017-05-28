47de3c5944825634fe47f0cf77abb6819be36e22
Elizabeth Merritt <lmerritt11@gmail.com>
2017-05-28 10:59:15 -0400

    changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,17 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
-  console.log()
-});
+// $(document).ready(function(){
+
+console.log('hi')
+  // $(".owl-carousel").owlCarousel();
+  // console.log()
 
 var slider = new IdealImageSlider.Slider({
   selector: '#slider',
-  height: auto, // Required but can be set by CSS
-  interval: 4000
+  height: 300,
+  interval: 2500,
+  transitionDuration:1500,
 });
+// new IdealImageSlider.Slider('#slider');
+
 slider.start();
+  
+
